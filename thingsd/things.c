bded4c93615154601875aeb9c29ce976afc2948e
Tracey Emery <tracey@traceyemery.net>
2019-04-29 14:30:07 -0600

    Missed one item for gcc 8.3.0 fix

--- thingsd/things.c
+++ thingsd/things.c
@@ -182,7 +182,7 @@ thgs_dispatch_main(int fd, short event, void *bula)
 					break;
 				if (strncmp(clt->name, imsg.data, BUFF) == 0) {
 					log_debug("Control killed client: %s",
-					    imsg.data);
+					    (char *)imsg.data);
 					clt_del(pthgsd, clt);
 					break;
 				}
