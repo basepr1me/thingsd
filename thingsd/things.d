550efb49582b04a26e3e57eeec6c4bd609152e20
Tracey Emery <tracey@traceyemery.net>
2019-08-21 15:22:21 -0600

    Inital bkup commit

---
+++ thingsd/things.d
@@ -0,0 +1,108 @@
+things.o: things.c /usr/include/sys/types.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/sys/endian.h \
+  /usr/include/sys/_endian.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/machine/endian.h \
+  /usr/include/err.h /usr/include/errno.h /usr/include/sys/errno.h \
+  /usr/include/event.h /usr/include/sys/time.h /usr/include/sys/select.h \
+  /usr/include/time.h /usr/include/sys/_null.h /usr/include/sys/_time.h \
+  /usr/include/sys/queue.h /usr/include/stdarg.h /usr/include/stdint.h \
+  /usr/include/pwd.h /usr/include/stdlib.h /usr/include/string.h \
+  /usr/include/strings.h /usr/include/syslog.h /usr/include/unistd.h \
+  /usr/include/sys/unistd.h \
+  /home/basepr1me/Documents/git/thingsd/thingsd/thingsd.h \
+  /usr/include/imsg.h /usr/include/pthread.h /usr/include/sys/signal.h \
+  /usr/include/machine/signal.h /usr/include/sys/siginfo.h \
+  /usr/include/limits.h /usr/include/sys/limits.h \
+  /usr/include/machine/limits.h /usr/include/sys/syslimits.h \
+  /usr/include/sched.h /usr/include/signal.h /usr/include/stdbool.h \
+  /usr/include/tls.h /usr/include/stddef.h \
+  /home/basepr1me/Documents/git/thingsd/thingsd/log.h \
+  /home/basepr1me/Documents/git/thingsd/thingsd/things.h
+
+/usr/include/sys/types.h:
+
+/usr/include/sys/cdefs.h:
+
+/usr/include/machine/cdefs.h:
+
+/usr/include/sys/endian.h:
+
+/usr/include/sys/_endian.h:
+
+/usr/include/sys/_types.h:
+
+/usr/include/machine/_types.h:
+
+/usr/include/machine/endian.h:
+
+/usr/include/err.h:
+
+/usr/include/errno.h:
+
+/usr/include/sys/errno.h:
+
+/usr/include/event.h:
+
+/usr/include/sys/time.h:
+
+/usr/include/sys/select.h:
+
+/usr/include/time.h:
+
+/usr/include/sys/_null.h:
+
+/usr/include/sys/_time.h:
+
+/usr/include/sys/queue.h:
+
+/usr/include/stdarg.h:
+
+/usr/include/stdint.h:
+
+/usr/include/pwd.h:
+
+/usr/include/stdlib.h:
+
+/usr/include/string.h:
+
+/usr/include/strings.h:
+
+/usr/include/syslog.h:
+
+/usr/include/unistd.h:
+
+/usr/include/sys/unistd.h:
+
+/home/basepr1me/Documents/git/thingsd/thingsd/thingsd.h:
+
+/usr/include/imsg.h:
+
+/usr/include/pthread.h:
+
+/usr/include/sys/signal.h:
+
+/usr/include/machine/signal.h:
+
+/usr/include/sys/siginfo.h:
+
+/usr/include/limits.h:
+
+/usr/include/sys/limits.h:
+
+/usr/include/machine/limits.h:
+
+/usr/include/sys/syslimits.h:
+
+/usr/include/sched.h:
+
+/usr/include/signal.h:
+
+/usr/include/stdbool.h:
+
+/usr/include/tls.h:
+
+/usr/include/stddef.h:
+
+/home/basepr1me/Documents/git/thingsd/thingsd/log.h:
+
+/home/basepr1me/Documents/git/thingsd/thingsd/things.h:
